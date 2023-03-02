ba3c659088dba6c14b0718f7fc8245166f855806
joelstoppa <joel.stoppa@gmail.com>
2023-03-02 03:14:50 +0100

    Add displayController module

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const gameBoard = (() => {
-  let board = [null, null, null, null, null, null, null, null, null];
+  let board = ["X", "O", "O", "X", "X", "X", "O", "O", "X"];
 
   return { board };
 })();
 
+console.log(gameBoard.board);
+
 const playerFactory = (name, symbol) => {
   return { name, symbol };
 };
+
+const displayController = ((board) => {
+  let cells = document.querySelectorAll(".cell");
+
+  const populateCells = () => {
+    cells.forEach((cell, index) => {
+      cell.textContent = board[index];
+    });
+    console.log(cells.length);
+  };
+
+  return { populateCells };
+})(gameBoard.board);
+
+displayController.populateCells();
